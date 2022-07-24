cd0c206a6f51952860bf3d43da8d0342a9516611
Carlos Eduardo <cpwaldow@gmail.com>
2022-07-24 19:33:02 -0300

    add lazy loading to Photo component

--- Components/Photo.js
+++ Components/Photo.js
@@ -5,7 +5,7 @@ export default function Photo({ props }) {
   return (
     <div className={styles.column}>
       <div className={styles.card}>
-        <Image src={props.image[props.name]} alt={props.name} />
+        <Image src={props.image[props.name]} alt={props.name} loading="lazy" />
         <div className="container">
           <h2 className={styles.name}>{props.name}</h2>
         </div>
