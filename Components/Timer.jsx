0b580627b242b242d0e6a767e8ad4894fbe3bff0
Carlos Eduardo <cpwaldow@gmail.com>
2023-06-14 12:03:49 -0300

    adicionando validação para mostrar ou não o tempo de descanso

--- Components/Timer.jsx
+++ Components/Timer.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 import timerStyle from '../styles/Timer.module.css';
 
 const Timer = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(false);
   useEffect(() => {
     const timer =
       counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    if (counter === 0) return setCounter(false);
     return () => clearInterval(timer);
   }, [counter]);
   return (
     <div className={timerStyle['timer-container']}>
-      <h3 className={timerStyle['timer-title']}>Descanso: {counter}s</h3>
+      {counter && (
+        <h3 className={timerStyle['timer-title']}>Descanso: {counter}s</h3>
+      )}
       <button
         className={timerStyle['timer-btn']}
         onClick={() => setCounter(60)}
